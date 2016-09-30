a032e6e9c6e62cac9e00cfc4d876039366a049dc
Alex Malkevich <malkevich.alex@gmail.com>
2016-09-30 15:12:35 +0400

    Fix providers.ts to properly generate *.d.ts

--- src/http/providers.ts
+++ src/http/providers.ts
@@ -1,7 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+// noinspection ES6UnusedImports
+import { Http, RequestOptions, XHRBackend } from '@angular/http';
 import { HttpInterceptorService } from './http-interceptor.service';
+// noinspection ES6UnusedImports
 import {
   InterceptableHttpProxyProviders,
-  InterceptableHttpProxyNoOverrideProviders
+  InterceptableHttpProxyNoOverrideProviders,
+  InterceptableHttpProxyService
 } from './interceptable-http-proxy.service';
 import { InterceptableHttpProviders } from './interceptable-http';
 import { InterceptableStoreFactory } from './interceptable-store';
@@ -12,11 +18,13 @@ const SharedProviders = [
   ...InterceptableHttpProviders
 ];
 
+// noinspection JSUnusedGlobalSymbols
 export const HTTP_INTERCEPTOR_PROVIDER = [
   ...SharedProviders,
   ...InterceptableHttpProxyProviders
 ];
 
+// noinspection JSUnusedGlobalSymbols
 export const HTTP_INTERCEPTOR_NO_OVERRIDE_PROVIDER = [
   ...SharedProviders,
   ...InterceptableHttpProxyNoOverrideProviders
