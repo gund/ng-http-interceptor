dad89f58abfe8e67ed7a70d1c5c41127eb4f58a3
gund <malkevich.alex@gmail.com>
2017-01-09 21:01:17 +0400

    fix(exports): Remove providers from exports
    This is a breaking change but we refer this to v2

--- src/http/index.ts
+++ src/http/index.ts
@@ -5,5 +5,4 @@ export * from './interceptable';
 export * from './interceptable-http';
 export * from './http-interceptor';
 export * from './http-interceptor.service';
-export * from './providers';
 export * from './helpers/index';
