4e7adb578225409fdb95c5f48f708cbc2466f0ac
gund <malkevich.alex@gmail.com>
2017-02-17 00:56:30 +0400

    build(rollup): Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,22 @@
+import { globalsRegex, GLOBAL } from 'rollup-globals-regex';
+
 export default {
     entry: 'dist/index.js',
     dest: 'dist/bundles/ng-http-interceptor.umd.js',
     format: 'umd',
     moduleName: 'httpInterceptor',
-    globals: {
-        '@angular/core': 'ng.core',
-        '@angular/http': 'ng.http',
-        'rxjs/Observable': 'Rx',
-        'rxjs/add/observable/of': 'Rx.Observable',
-        'rxjs/add/observable/empty': 'Rx.Observable',
-        'rxjs/add/operator/switchMap': 'Rx.Observable.prototype',
-        'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-    },
-    external: [
-        '@angular/core',
-        '@angular/http',
-        'rxjs/Observable',
-        'rxjs/add/observable/of',
-        'rxjs/add/observable/empty',
-        'rxjs/add/operator/switchMap',
-        'rxjs/add/operator/mergeMap',
-    ]
+    globals: globalsRegex({
+        'tslib': 'tslib',
+        [GLOBAL.NG2]: GLOBAL.NG2.TPL,
+        [GLOBAL.RX]: GLOBAL.RX.TPL,
+        [GLOBAL.RX_OBSERVABLE]: GLOBAL.RX_OBSERVABLE.TPL,
+        [GLOBAL.RX_OPERATOR]: GLOBAL.RX_OPERATOR.TPL,
+    }),
+    external: (moduleId) => {
+        if (/^(\@angular|rxjs)\//.test(moduleId)) {
+            return true;
+        }
+
+        return false;
+    }
 }
