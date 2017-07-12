c9c011910e210c49e16a0db54f4babdefec2b3cf
gund <malkevich.alex@gmail.com>
2017-07-12 22:05:54 +0400

    fix(http-interceptor): Add boolean to possible return types of Request interceptor
    closes #160

--- src/http/http-interceptor.ts
+++ src/http/http-interceptor.ts
@@ -2,7 +2,7 @@ import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Interceptable, Interceptor } from './interceptable';
 
-export type RequestInterceptor = Interceptor<any[], any[] | Observable<any[]>>;
+export type RequestInterceptor = Interceptor<any[], any[] | Observable<any[]> | boolean>;
 export type ResponseInterceptor = Interceptor<Observable<Response>, Observable<Response>>;
 
 export interface HttpInterceptor {
